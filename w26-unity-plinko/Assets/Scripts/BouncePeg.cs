b130709c0b885960471a4da2ec454e027ac5c0cb
Pay-ton <001224922@mohawkcollege.ca>
2026-04-16 17:20:06 -0400

    peg bouncing, script location shuffling, basic score function formatting

---
+++ w26-unity-plinko/Assets/Scripts/BouncePeg.cs
@@ -0,0 +1,42 @@
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class BouncePeg : MonoBehaviour
+{
+    public int points = 1;
+    public Score score;
+    
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // This is to make sure that the pegs bounce.
+        if (collision != null)
+        {
+            Vector2 Bounce = collision.gameObject.GetComponent<Rigidbody2D>().linearVelocity;
+            Bounce = Bounce.normalized;
+            Bounce *= 2;
+
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Bounce, ForceMode2D.Impulse);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // More Peg bouncing
+            //if (Collision != null)
+
+
+
+    }
+}
--- w26-unity-plinko/Assets/Scripts/BumperLeft.cs
+++ w26-unity-plinko/Assets/Scripts/BumperLeft.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
 
-public class BouncePeg : MonoBehaviour
+public class BumperLeft : MonoBehaviour
 {
+    public int score;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
