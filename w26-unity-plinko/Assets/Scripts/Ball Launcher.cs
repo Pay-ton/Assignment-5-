a7ab43c580f16e4c008ee6dfe76b659f9ab3d1d7
Pay-ton <001224922@mohawkcollege.ca>
2026-04-16 16:15:40 -0400

    level geometry fixes, previous commit was partly lying, ball launcher refined into ideal state

--- w26-unity-plinko/Assets/Scripts/Ball Launcher.cs
+++ w26-unity-plinko/Assets/Scripts/Ball Launcher.cs
@@ -5,41 +5,42 @@ public class BallLauncher : MonoBehaviour
 {
     public float LaunchSpeed = 20.0f;
     Vector3 position;
-    public Vector3 startPosition; 
+    public Vector3 startPosition;
     public Quaternion startRotation;
     public Vector3 endPosition;
     public Quaternion endRotation;
-    public GameObject BallLauncherSquare;
-    public GameObject Disc; 
+    public GameObject Disc;
+    public GameObject InLaunchPosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         transform.position = startPosition;
         transform.rotation = startRotation;
-}
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Disc = GameObject.Find("Disc(Clone)");
+
+
         // This should allow the ball to spawn and launch when spacebar is pressed
-        if (Input.GetKey(KeyCode.Space))
+        if (Disc != null && Input.GetKey(KeyCode.Space) && InLaunchPosition.GetComponent<Collider2D>().IsTouching(Disc.GetComponent<Collider2D>()))
+
         {
-            Disc = GameObject.Find("Disc(Clone)");
 
-
-            transform.position = Vector3.Lerp(transform.position, endPosition, LaunchSpeed * Time.deltaTime); 
-            Disc.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 50)); 
+            transform.position = Vector3.Lerp(transform.position, endPosition, LaunchSpeed * Time.deltaTime);
+            Disc.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 500));
         }
         else
         {
-            
+
             //This should make it go back to start when the spacebar isn't being held. 
             transform.position = startPosition;
             transform.rotation = startRotation;
         }
-        
+
 
     }
 }
